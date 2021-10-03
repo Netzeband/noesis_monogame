9e0aefe53e706ada2cdc6ea1c7cedd7d6353ab0d
Andre Netzeband <a@net.ba>
2021-10-03 14:26:00 +0200

    Added Monogame template for DirectX.

--- source/NoesisMonogame/NoesisMonogame/Game1.cs
+++ source/NoesisMonogame/NoesisMonogame/Game1.cs
@@ -19,5 +19,46 @@ namespace NoesisMonogame
             IsMouseVisible = true;
         }
 
+        protected override void Initialize()
+        {
+            // initialize your code here ...
+            
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+            // load any content here ...
+        }
+
+        protected override void UnloadContent()
+        {
+            // unload content here if necessary ...
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if ((GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed) ||
+                (Microsoft.Xna.Framework.Input.Keyboard.GetState().IsKeyDown(Keys.Escape)) )
+            {
+                Exit();
+            }
+            
+            // update your game state here ...
+            
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // draw your game state here ...
+            
+            base.Draw(gameTime);
+        }
+
     }
 }
